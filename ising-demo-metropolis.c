515bc213cdc58c7fce8c2e4c6f7e10a5782ac241
eddy-anderson <eddyanderson13@gmail.com>
2015-12-21 20:29:32 -0500

    First commit

---
+++ ising-demo-metropolis.c
@@ -0,0 +1,116 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "ising_datastructure.h"
+#include "ising.h"
+#include "metropolis.h"
+#include "progressbar.h"
+#include "ising-demo-metropolis.h"
+
+int main (int argc, char **argv)
+{
+    struct ising model;
+
+    double tmax = 5., tmin = 3., t;     // temperature (kT)
+    double h = 0.;              // magnetic field
+    double j = 1.;              // ferromagnetic coupling
+
+    int lx = 32;
+    int ly = lx;
+    int np = 40;
+    double tstep = (tmax - tmin) / (np - 1);    //temperature step for scan
+
+    int mcsteps = 1024 * 128;   // 2^{17}
+
+    print_header (&model, tmin, tmax, stderr);
+
+
+    t = tmin;
+
+    if (ising_init (&model, lx, ly, j, h, t) != 0)      //error if faulty model
+    {
+        fprintf (stderr, "%s - cannot allocate spin array\n", argv[0]);
+        argc = 1;
+        exit (1);
+    }
+
+    print_header (&model, tmin, tmax, stderr);
+
+    int therm_steps = mcsteps / 4;
+    int report = therm_steps / 32;
+
+
+    for (int i = 0; i < np; i++)
+    {
+
+
+        for (int s = 1; s <= therm_steps; s++)  //loop for thermal phase progress bar
+        {
+            (void) one_metropolis_step_per_spin (&model);
+            if (s % report == 0)
+            {
+                text_progress_bar (s, therm_steps, 32, "Thermalization",
+                    stderr);
+            }
+        }
+        fprintf (stderr, " Done\n");
+
+        report = mcsteps / 32;
+        double mav = 0., m2av = 0., eav = 0., e2av = 0.;
+        double cv, xi;
+
+        for (int q = 1; q <= mcsteps; q++)      //setting spin and progress bar
+        {
+            (void) one_metropolis_step_per_spin (&model);
+            double m = magnetization_per_spin (&model);
+            double e = energy_per_spin (&model);
+
+            mav += m;
+            m2av += m * m;
+            eav += e;
+            e2av += e * e;
+
+            if (q % report == 0)
+            {
+                text_progress_bar (q, mcsteps, 32, "Production", stderr);
+            }
+
+        }
+        fprintf (stderr, " Done\n\n");
+
+        mav /= mcsteps;
+        eav /= mcsteps;
+        m2av /= mcsteps;
+        e2av /= mcsteps;
+
+        // specific heat dE/dt
+        cv = (e2av - eav * eav) / (t * t);
+
+        // magnetic susceptibility
+        xi = (m2av - mav * mav) / t;
+
+        printf ("%.3f   %.3f     % f  %f     % f  %f\n", t, h, mav, xi, eav,
+            cv);
+
+        ising_reinit (&model, h, t);
+        t += tstep;             //increment t up
+
+    }
+
+    ising_free (model);
+
+    return 0;
+
+
+}
+
+void print_header (struct ising *model, double tmin, double tmax, FILE * fp)
+{
+    fprintf (fp, "\n");
+    fprintf (fp, " Two-dimensional Ising model - Metropolis simulation\n");
+    fprintf (fp, " ---------------------------------------------------\n");
+    fprintf (fp,
+        " Tmin/J = %.3f  Tmax/J = %.3f   H/J = %.3f  %dx%d		\n	\n",
+        tmin / model->J, tmax / model->J, model->H / model->J,
+        model->Lx, model->Ly);
+}
---
+++ ising-model.gp
@@ -0,0 +1,6 @@
+set t png
+set o "ising-model.png"
+
+set key off
+
+p "res" u 1:6 w l
